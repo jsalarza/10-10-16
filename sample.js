837705f8094e7e8c7c7e83156be883c2191c1bc1
Jim Salarza <salarzajim@gmail.com>
2016-10-10 19:43:20 +0800

    Add files via upload

--- sample.js
+++ sample.js
@@ -11,17 +11,3 @@ driver.findElement(By.name('q')).sendKeys('webdriver');
 driver.findElement(By.name('btnG')).click();
 driver.wait(until.titleIs('webdriver - Google Search'), 1000);
 driver.quit();
-
-function check_title(){
-	var promise = driver.getTitle().then( function (title){
-	if(title === 'wiki - Google Search'){
-		console.log('success');
-		return true;
-	}
-	else{
-		console.log('fail --' + title);
-	}
-
-	});
-	return promise;
-}
